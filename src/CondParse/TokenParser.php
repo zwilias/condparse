e5166bb3b8592d930bc34e1d2f590cdf7fcfe0f7
Van Peer Ilias <iliasvp@hi68229.local>
2016-03-06 13:05:05 +0100

    if with return -> if/elif

--- src/CondParse/TokenParser.php
+++ src/CondParse/TokenParser.php
@@ -16,40 +16,30 @@ class TokenParser
     public function parseToken($token, $value, OperandStack $operandStack, \SplStack $operatorStack, TokenMap $tokenMap)
     {
         if ($token === TokenMap::TOKEN_WHITESPACE) {
-            return;
-        }
-
-        if ($tokenMap->isOperand($token)) {
+            // skip whitespace
+        } elseif ($tokenMap->isOperand($token)) {
             $operandStack->push(
                 $tokenMap->buildOperand($token, $value)->consumeTokens($operandStack)
             );
-            return;
-        }
-
-        if (
+        } elseif (
             $token === TokenMap::TOKEN_BRACKET_OPEN
             || $operatorStack->isEmpty()
             || $tokenMap->compareOperatorPrecedence($token, $operatorStack->top()[0]) > 0
         ) {
             $operatorStack->push([$token, $value]);
-            return;
-        }
-
-        if ($token === TokenMap::TOKEN_BRACKET_CLOSE) {
+        } elseif ($token === TokenMap::TOKEN_BRACKET_CLOSE) {
             while ($operatorStack->top()[0] !== TokenMap::TOKEN_BRACKET_OPEN) {
                 $this->pushOperand($operandStack, $operatorStack, $tokenMap);
             }
 
             $operatorStack->pop();
-            return;
-        }
+        } else {
+            while (! $operatorStack->isEmpty() && $tokenMap->compareOperatorPrecedence($token, $operatorStack->top()[0]) <= 0) {
+                $this->pushOperand($operandStack, $operatorStack, $tokenMap);
+            }
 
-        while (! $operatorStack->isEmpty() && $tokenMap->compareOperatorPrecedence($token, $operatorStack->top()[0]) <= 0) {
-            $this->pushOperand($operandStack, $operatorStack, $tokenMap);
+            $operatorStack->push([$token, $value]);
         }
-
-        $operatorStack->push([$token, $value]);
-        return;
     }
 
 
